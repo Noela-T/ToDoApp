d95ed6ef67116393037867e77416cc226e88f462
Noela-T <tenkunoela@gmail.com>
2018-07-17 23:03:11 +0100

    Added deleteTask function, which when called deletes items from to-do list

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # ToDoApp
 A python console application that creates a To-Do list.
-Function addTask adds a new task and the time for that task to you list and then prints your list
+
+To run this application, you would need to have a Python interpreter installed
+on your machine. Specifically python 3.x versions.
+
+Make sure you can run python files from your terminal.
+
+In your terminal, navigate to where the file is saved and type python3 todoapp.py
+to run the application.
+
+The program allows you to create a to-do list, and also add and delete items to
+and from the list respectively.
--- todoapp.py
+++ todoapp.py
@@ -1,19 +1,50 @@
 from datetime import datetime
 from datetime import date
 
+indexDict={}
+timeDict={}
 tasks={}
+index = 1
 def addTask(task,time):
+    global index
+    indexD=1
     #converts time to a datetime object to enable sorting of time
     ctime=datetime.strptime(time,"%H:%M")
-    tasks[ctime]=task
-    #sorts through the dictionary but returns a list object
+    tasks[index]=task
+    timeDict[ctime]=index
+    index += 1
+    #sorts through the dictionaries but returns a list object
     sortedtasks=sorted(tasks.items())
+    sortedtime=sorted(timeDict.items())
+    for i in sortedtime:
+        indexDict[indexD]=i[0]
+        indexD += 1
     print ("*****MY TO-DO LIST*****")
-    for i in sortedtasks:
-        print ("  " + i[0].strftime("%H:%M")+" -"+i[1])
+    for i in sortedtime:
+        for j in sortedtasks:
+            if (i[1]==j[0]):
+                for key, value in indexDict.items():
+                    if (i[0]==indexDict[key]):
+                        print ("  "+str(key)+". " + i[0].strftime("%H:%M")+" -" + j[1])
     return 0
-    
+
+def deleteTask(num):
+    num=int(num)
+    for key, value in indexDict.copy().items():
+        if (num == key):
+            for timekey, value in timeDict.copy().items():
+                if (timekey==indexDict[key]):
+                    for taskkey, value in tasks.copy().items():
+                        if (taskkey==timeDict[timekey]):
+                            del tasks[taskkey]
+                    del timeDict[timekey]
+            del indexDict[key]
+
 while True:
     name=input("Enter new task: ");
     time=input("Time for task(e.g 19:30): ")
     addTask(name,time)
+    deleteinput=input("Delete any completed task? Yes/No : ")
+    if deleteinput == 'Yes':
+        numToDelete=input("Enter task number: ")
+        deleteTask(numToDelete)
